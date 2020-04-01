8e67db7841ce2c3a6e5e7004886b9d7f2c04f72a
Tillo <tillo.bosshart@gmail.com>
2020-04-01 07:30:51 +0200

    WIP

--- parser/parser.go
+++ parser/parser.go
@@ -5,15 +5,35 @@ import (
 	"strings"
 
 	"github.com/morgulbrut/color256"
+	"github.com/morgulbrut/patchbookGo/patch"
 )
 
-func File(lines []string) Patch {
+const CONN = "-"
+const CONN_CV = ">>"
+const CONN_AUDIO = "->"
+const CONN_PITCH = "p>" //1v/oct or Hz/V
+const CONN_GATE = "g>"
+const CONN_TRIGGER = "g>"
+const CONN_CLOCK = "g>"
+
+func File(s []string) patch.Patch {
+	var p patch.Patch
+
 	color256.PrintHiOrange("Parsing...:")
-	for i, line := range lines {
+	for i, line := range s {
 		fmt.Println(i, line)
 		line = strings.TrimSpace(line)
-		if strings.HasPrefix(line,"*")
+		if strings.HasPrefix(line, CONN) {
+			p.Conns = append(p.Conns, connection(line))
+		}
 	}
+	return p
+}
 
-	return Patch{}
+func connection(s string) patch.Connection {
+	var c patch.Connection
+	if strings.Contains(s, CONN_CV) {
+		c.Type = "CV"
+	}
+	return c
 }
