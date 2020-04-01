70339a5b584ca1942935b78beb91bb60eeabef23
Tillo <tillo.bosshart@gmail.com>
2020-04-01 17:21:41 +0200

    added Stringer

--- patch/patch.go
+++ patch/patch.go
@@ -1,8 +1,14 @@
 package patch
 
+import "fmt"
+
 type Patch struct {
 	Conns []Connection
 	Sets  []Settings
+}
+
+func (p Patch) String() string {
+	return fmt.Sprintf("Connections: \n%v \nSettings: \n%v", p.Conns, p.Sets)
 }
 
 type Connection struct {
@@ -11,9 +17,17 @@ type Connection struct {
 	Type   string
 }
 
+func (c Connection) String() string {
+	return fmt.Sprintf("\tSource: %v, Destination: %v, Type: %s \n", c.Source, c.Dest, c.Type)
+}
+
 type Port struct {
 	Device   string
 	PortName string
+}
+
+func (p Port) String() string {
+	return fmt.Sprintf("%s (%s)", p.Device, p.PortName)
 }
 
 type Settings struct {
@@ -21,7 +35,15 @@ type Settings struct {
 	Sets   []Setting
 }
 
+func (s Settings) String() string {
+	return fmt.Sprintf("\n\tDevice: %v \n\t\tSettings: \n%v", s.Device, s.Sets)
+}
+
 type Setting struct {
 	Parameter string
 	Value     string
+}
+
+func (s Setting) String() string {
+	return fmt.Sprintf("\t\t%s = %s\n", s.Parameter, s.Value)
 }
